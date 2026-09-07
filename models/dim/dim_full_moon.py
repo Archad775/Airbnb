76bd8981ed6b71be6069fb2c8cd3c851aaadb797
Archad775 <116772817+Archad775@users.noreply.github.com>
2026-09-07 14:43:35 +0100

    Introduction of snowpark

--- models/dim/dim_full_moon.py
+++ models/dim/dim_full_moon.py
@@ -1,22 +1,17 @@
-import holidays
-
-def is_holiday(date_col):
-    german_holidays =holidays.Germany()
-    is_holiday = (date_col in german_holidays)
-    return is_holiday
+from snowflake.snowpark.functions import udf, col
+from snowflake.snowpark.types import BooleanType, DateType
 
 def model(dbt, session):
     dbt.config(
-        materialized = 'table',
-        packages = ["holidays", "pandas"],
-        full_refresh = True
+        materialized='table',
+        packages=["holidays"]
     )
 
+    @udf(return_type=BooleanType(), input_types=[DateType()], packages=["holidays"])
+    def is_holiday(date_val):
+        import holidays
+        german_holidays = holidays.Germany()
+        return date_val in german_holidays
 
     orders_df = dbt.ref("seed_full_moon_dates")
-
-    df = orders_df.to_pandas()
-
-    df["IS_HOLIDAY"] = df["FULL_MOON_DATE"].apply(is_holiday)
-
-    return df
+    return orders_df.with_column("IS_HOLIDAY", is_holiday(col("FULL_MOON_DATE")))
